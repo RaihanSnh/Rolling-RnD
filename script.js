8ef02ba265c3d1dd302a84e84003a5ca9b7c3699
RaihanSnh <natharaihans@gmail.com>
2024-11-28 23:26:27 +0700

    fix: login and some addition on home

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-window.onload = function() {
+window.onload = function () {
+    // Handle login status
     if (window.location.pathname.includes('login.html')) {
         const isLoggedIn = sessionStorage.getItem('isLoggedIn');
         if (isLoggedIn === 'true') {
             window.location.replace('home.html');
         }
 
-        document.getElementById('loginForm').addEventListener('submit', handleLogin);
+        document.getElementById('login-form').addEventListener('submit', handleLogin);
     }
 
     if (window.location.pathname.includes('home.html')) {
@@ -17,26 +18,40 @@ window.onload = function() {
             document.getElementById('welcomeUser').textContent = `Welcome, ${username}`;
 
             document.getElementById('logoutBtn').addEventListener('click', logout);
+
+            document.getElementById('search-form').addEventListener('submit', function (event) {
+                event.preventDefault();
+                const query = document.getElementById('search-box').value.toLowerCase();
+                const members = document.querySelectorAll('.member');
+                members.forEach(member => {
+                    const name = member.getAttribute('data-name').toLowerCase();
+                    if (name.includes(query)) {
+                        member.style.display = 'block';
+                    } else {
+                        member.style.display = 'none';
+                    }
+                });
+            });
         }
     }
-}
+};
 
 function handleLogin(event) {
     event.preventDefault();
-    
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const errorMsg = document.getElementById('errorMessage');
-    
-    if (username === "Calon RnD" && password === "akumaurnd123") {
+    const errorMsg = document.getElementById('error-message');
+
+    if (username === "CalonRnD" && password === "akumaurnd123") {
         sessionStorage.setItem('isLoggedIn', 'true');
         sessionStorage.setItem('username', username);
         window.location.replace('home.html');
     } else {
         errorMsg.style.display = 'block';
-        document.getElementById('loginForm').reset();
-        setTimeout(function() {
-            errorMessage.style.display = 'none';
+        document.getElementById('login-form').reset();
+        setTimeout(function () {
+            errorMsg.style.display = 'none';
         }, 2000);
     }
 }
